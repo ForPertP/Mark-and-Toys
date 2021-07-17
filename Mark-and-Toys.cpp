06f08d18ea17a70b07a619f3877d95c4d7bb43ea
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-07-18 07:45:35 +0900

    Update Mark-and-Toys.cpp

--- Mark-and-Toys.cpp
+++ Mark-and-Toys.cpp
@@ -1,1 +1,20 @@
+int maximumToys(vector<int> prices, int k)
+{
+    int count = 0;
+    long long int total = 0;
+
+    std::sort(prices.begin(), prices.end());
+
+    for (int i = 0; i < prices.size(); ++i)
+    {
+        total += prices[i];
+        
+        if (total <= k)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
 
