c8673be8b423eb7a22977b856c0a4c6d5859f245
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-02-08 04:14:19 +0900

    Update Mark-and-Toys.cpp

--- Mark-and-Toys.cpp
+++ Mark-and-Toys.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'maximumToys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY prices
+ *  2. INTEGER k
+ */
+
 int maximumToys(vector<int> prices, int k)
 {
     int count = 0;
@@ -5,7 +22,8 @@ int maximumToys(vector<int> prices, int k)
 
     std::sort(prices.begin(), prices.end());
 
-    for (int i = 0; i < prices.size(); ++i)
+    int pricesSize = prices.size();
+    for (int i = 0; i < pricesSize; ++i)
     {
         total += prices[i];
         
@@ -18,3 +36,6 @@ int maximumToys(vector<int> prices, int k)
     return count;
 }
 
+int main()
+{
+}
