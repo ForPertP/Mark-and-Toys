04b14f6de2e15b68ed8d3bc800e3ac16f9fbf253
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-02-13 04:44:05 +0900

    Update Mark-and-Toys.java

--- Mark-and-Toys.java
+++ Mark-and-Toys.java
@@ -20,4 +20,32 @@ class Result {
      *  1. INTEGER_ARRAY prices
      *  2. INTEGER k
      */
+
+    public static int maximumToys(List<Integer> prices, int k) {
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
